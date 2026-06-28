c44462041b7747d6a7784ddd20f6cc5f55dfc77b
sarfaraj <sarfarajs778@gmail.com>
2026-06-28 17:35:38 +0530

    Completed Java Task 1 - Environment Setup and Basic Syntax
    Completed Task 1 of the Java Developer Internship.
    
    This project includes 10 Java programs covering core Java concepts:
    
    - Hello World
    - Temperature Converter
    - Even Odd Checker
    - Multiplication Table
    - Digit Counter
    - Fibonacci Series
    - Prime Number Checker
    - Day Finder
    - Array Operations
    - Circle Operations
    
    Concepts Covered:
    - Variables and Data Types
    - Operators
    - Conditional Statements
    - Loops
    - Arrays
    - Methods
    - Scanner Class
    - Math Class
    
    Developed using Java and tested in VS Code.

---
+++ TemperatureConverter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("Temperature in Celsius = %.2f°C", celsius);
+
+        sc.close();
+    }
+}
